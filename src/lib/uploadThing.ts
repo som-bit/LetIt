0b5a314a43e5c6bd9bed16406ab526ee10c8da0f
som-bit <sr8180525@gmail.com>
2023-09-13 17:22:38 +0530

    deployment fix

--- src/lib/uploadThing.ts
+++ src/lib/uploadThing.ts
@@ -1,4 +1,5 @@
-import { OurFileRouter } from '@/app/api/uploadthing/core'
 import { generateReactHelpers } from '@uploadthing/react/hooks'
+
+import type { OurFileRouter } from '@/app/api/uploadthing/core'
 
 export const { uploadFiles } = generateReactHelpers<OurFileRouter>()
