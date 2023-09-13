6613a731c118711a06c155cd1f51622ae73bb4f1
som-bit <sr8180525@gmail.com>
2023-09-13 17:27:49 +0530

    deployment fix

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,7 +3,6 @@ import { createUploadthing, type FileRouter } from 'uploadthing/next'
 
 const f = createUploadthing()
 
-
 export const ourFileRouter = {
     imageUploader: f({ image: { maxFileSize: '4MB' } })
         .middleware(async (req) => {
